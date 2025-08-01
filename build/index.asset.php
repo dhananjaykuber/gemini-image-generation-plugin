8dccea3f3832cd219f7c499a11e4fa2a0584e3dd
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-08-02 00:18:22 +0530

    build: Add assets build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1dbc560d33c20f8dc60a');
---
+++ build/js/media-frame.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '08b2e2d1ca961fa9c4e6');
