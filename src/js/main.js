dc1b9a3fc2aa5416757885d5757292a5f13a637f
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-08-02 00:22:16 +0530

    format: Format js scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,111 @@
 import { __ } from '@wordpress/i18n';
 
-jQuery(document).ready(function ($) {
-    // Handle the clear button click
-    $(document).on('click', '#gemini-clear-button', function () {
-        $('#gemini_prompt').val('');
-        $('#gemini-preview').empty();
-    });
+jQuery( document ).ready( function ( $ ) {
+	// Handle the clear button click
+	$( document ).on( 'click', '#gemini-clear-button', function () {
+		$( '#gemini_prompt' ).val( '' );
+		$( '#gemini-preview' ).empty();
+	} );
 
-    // Handle the image generation button click
-    $(document).on('click', '#gemini-generate-button', function () {
-        var $this = $(this);
+	// Handle the image generation button click
+	$( document ).on( 'click', '#gemini-generate-button', function () {
+		const $this = $( this );
 
-        var prompt = $('#gemini_prompt').val();
-        if (!prompt) {
-            alert(__('Please enter a prompt.'));
-            return;
-        }
+		const prompt = $( '#gemini_prompt' ).val();
+		if ( ! prompt ) {
+			alert( __( 'Please enter a prompt.' ) );
+			return;
+		}
 
-        const $preview = $('#gemini-preview');
-        $preview.empty();
+		const $preview = $( '#gemini-preview' );
+		$preview.empty();
 
-        $this.prop('disabled', true);
-        $this.text(__('Generating...'));
+		$this.prop( 'disabled', true );
+		$this.text( __( 'Generating…' ) );
 
-        $.post(
-            geminiImgGen.ajaxURL,
-            {
-                action: 'gemini_img_generate_image',
-                prompt: prompt,
-                nonce: geminiImgGen.nonce,
-            },
-            function (response) {
-                if (!response.success) {
-                    alert(__('Error: ') + response.data.message);
-                    return;
-                }
+		$.post(
+			geminiImgGen.ajaxURL,
+			{
+				action: 'gemini_img_generate_image',
+				prompt,
+				nonce: geminiImgGen.nonce,
+			},
+			function ( response ) {
+				if ( ! response.success ) {
+					alert( __( 'Error:' ) + response.data.message );
+					return;
+				}
 
-                const $infoMessage = $('<span>')
-                    .text(__('Click on the image to upload'))
-                    .attr('class', 'gemini-info-message');
+				const $infoMessage = $( '<span>' )
+					.text( __( 'Click on the image to upload' ) )
+					.attr( 'class', 'gemini-info-message' );
 
-                const $btn = $('<button>')
-                    .attr('id', 'gemini-image-button')
-                    .attr('data-title', response.data.title)
-                    .attr('data-alt', response.data.alt);
+				const $btn = $( '<button>' )
+					.attr( 'id', 'gemini-image-button' )
+					.attr( 'data-title', response.data.title )
+					.attr( 'data-alt', response.data.alt );
 
-                const $img = $('<img>')
-                    .attr('src', response.data.image)
-                    .attr('alt', response.data.alt || __('Generated Image'))
-                    .attr('class', 'gemini-generated-image');
+				const $img = $( '<img>' )
+					.attr( 'src', response.data.image )
+					.attr( 'alt', response.data.alt || __( 'Generated Image' ) )
+					.attr( 'class', 'gemini-generated-image' );
 
-                $btn.append($img);
+				$btn.append( $img );
 
-                $preview.empty().append($btn);
-                $preview.append($infoMessage);
-            }
-        ).always(function () {
-            $this.prop('disabled', false);
-            $this.text(__('Generate'));
-        });
-    });
+				$preview.empty().append( $btn );
+				$preview.append( $infoMessage );
+			}
+		).always( function () {
+			$this.prop( 'disabled', false );
+			$this.text( __( 'Generate' ) );
+		} );
+	} );
 
-    // Handle the image button click
-    $(document).on('click', '#gemini-image-button', function () {
-        const $this = $(this);
-        var title = $(this).data('title');
-        var alt = $(this).data('alt');
-        var imageSrc = $(this).find('img').attr('src');
-        var infoMessage = $('.gemini-info-message');
+	// Handle the image button click
+	$( document ).on( 'click', '#gemini-image-button', function () {
+		const $this = $( this );
+		const title = $( this ).data( 'title' );
+		const alt = $( this ).data( 'alt' );
+		const imageSrc = $( this ).find( 'img' ).attr( 'src' );
+		const infoMessage = $( '.gemini-info-message' );
 
-        $this.prop('disabled', true);
-        infoMessage.text(__('Inserting image...'));
-        infoMessage.addClass('spinner is-active');
+		$this.prop( 'disabled', true );
+		infoMessage.text( __( 'Inserting image…' ) );
+		infoMessage.addClass( 'spinner is-active' );
 
-        $.post(
-            geminiImgGen.ajaxURL,
-            {
-                action: 'gemini_img_upload_image',
-                title: title,
-                alt: alt,
-                image: imageSrc,
-                nonce: geminiImgGen.nonce,
-            },
-            function (response) {
-                if (!response.success) {
-                    alert(__('Error: ') + response.data.message);
-                    return;
-                }
+		$.post(
+			geminiImgGen.ajaxURL,
+			{
+				action: 'gemini_img_upload_image',
+				title,
+				alt,
+				image: imageSrc,
+				nonce: geminiImgGen.nonce,
+			},
+			function ( response ) {
+				if ( ! response.success ) {
+					alert( __( 'Error:' ) + response.data.message );
+					return;
+				}
 
-                const $message = $('<div>')
-                    .addClass('notice notice-success')
-                    .text(
-                        __(
-                            'Image inserted successfully! Please refresh the page and check the media library.'
-                        )
-                    );
+				const $message = $( '<div>' )
+					.addClass( 'notice notice-success' )
+					.text(
+						__(
+							'Image inserted successfully! Please refresh the page and check the media library.'
+						)
+					);
 
-                $('#gemini-preview').empty().append($message);
-            }
-        )
-            .fail(function () {
-                alert(__('Failed to insert image.'));
-            })
-            .always(function () {
-                $this.prop('disabled', false);
-                infoMessage.text(__('Click on the image to upload'));
-                infoMessage.removeClass('spinner is-active');
-            });
-    });
-});
+				$( '#gemini-preview' ).empty().append( $message );
+			}
+		)
+			.fail( function () {
+				alert( __( 'Failed to insert image.' ) );
+			} )
+			.always( function () {
+				$this.prop( 'disabled', false );
+				infoMessage.text( __( 'Click on the image to upload' ) );
+				infoMessage.removeClass( 'spinner is-active' );
+			} );
+	} );
+} );
