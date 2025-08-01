51e413ac43d001321fcf4f7b8d7c4b7941c9455d
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-08-02 00:14:46 +0530

    feat: Add webpack config for script

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * WordPress dependencies
+ */
+import defaultConfig from '@wordpress/scripts/config/webpack.config.js';
+
+// Extend the default config.
+const sharedConfig = {
+    ...defaultConfig,
+    output: {
+        path: path.resolve(process.cwd(), 'build', 'js'),
+        filename: '[name].js',
+        chunkFilename: '[name].js',
+    },
+    devtool: 'source-map',
+    optimization: {
+        ...defaultConfig.optimization,
+        splitChunks: {
+            ...defaultConfig.optimization.splitChunks,
+        },
+        minimizer: [...(defaultConfig.optimization?.minimizer || [])],
+    },
+};
+
+const jsFiles = {
+    ...sharedConfig,
+    entry: {
+        main: path.resolve(process.cwd(), 'src', 'js', 'main.js'),
+        'media-frame': path.resolve(
+            process.cwd(),
+            'src',
+            'js',
+            'media-frame.js'
+        ),
+    },
+};
+
+export default [jsFiles];
