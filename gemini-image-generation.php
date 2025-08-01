4fb0950658a6dbd2612691d4c64db7b7555c8a99
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-08-01 15:26:19 +0530

    feat: Setup plugin and add wp-scripts dependency

---
+++ gemini-image-generation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Gemini Image Generation
+ * Description:       A plugin that integrates Gemini AI for generating images based on user prompts, enhancing the content creation experience in WordPress.
+ * Plugin URI:        https://github.com/dhananjaykuber/gemini-image-generation-plugin
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Author:            Dhananjay Kuber
+ * Author URI:        https://dhananjaykuber.in
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       gemini-image-generation
+ *
+ * @package           gemini-image-generation
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
