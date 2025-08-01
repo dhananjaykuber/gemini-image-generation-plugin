47e52f69b1b78dd7df601e77c5a709441f33ffd5
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-08-02 00:15:27 +0530

    chore: Update script paths

--- gemini-image-generation.php
+++ gemini-image-generation.php
@@ -32,21 +32,29 @@ function gemini_img_gen_enqueue_scripts() {
 
 	wp_enqueue_style(
 		'gemini-img-gen-style',
-		GEMINI_IMG_GEN_FEATURES_URL . '/src/css/style.css',
+		GEMINI_IMG_GEN_FEATURES_URL . '/src/css/main.css',
 		array(),
 		GEMINI_IMG_GEN_FEATURES_VERSION
 	);
 
 	wp_enqueue_script(
-		'gemini-img-gen-script',
-		GEMINI_IMG_GEN_FEATURES_URL . '/build/index.js',
+		'gemini-img-gen-media-frame-script',
+		GEMINI_IMG_GEN_FEATURES_URL . '/build/js/media-frame.js',
 		array( 'jquery', 'media-views', 'wp-i18n' ),
 		GEMINI_IMG_GEN_FEATURES_VERSION,
 		true
 	);
 
+	wp_enqueue_script(
+		'gemini-img-gen-main-script',
+		GEMINI_IMG_GEN_FEATURES_URL . '/build/js/main.js',
+		array( 'jquery', 'wp-i18n' ),
+		GEMINI_IMG_GEN_FEATURES_VERSION,
+		true
+	);
+
 	wp_localize_script(
-		'gemini-img-gen-script',
+		'gemini-img-gen-main-script',
 		'geminiImgGen',
 		array(
 			'ajaxURL' => admin_url( 'admin-ajax.php' ),
